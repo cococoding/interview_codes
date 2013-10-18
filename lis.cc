f1320b80680419e3fd803233f8ce9888369b1ce4
cococoding <valorjohnny@gmail.com>
2013-10-18 14:31:47 +0800

    Update lis.cc

--- lis.cc
+++ lis.cc
@@ -14,17 +14,25 @@ struct node_t {
 }
 
 int f(node_t * root) {
-    if (root) return 0;
+    if (!root) return 0;
     root->lsi = 1;
-    if (root->lchild)
-        root->lsi += f(root->lchild->lchild)+f(root->lchild->rchild);
-    if (root->rchild)
-        root->lsi += f(root->rchild->lchild)+f(root->rchild->rchild);
-    return root->lsi;    
+    f(root->lchild);
+    f(root->rchild);
+        
+    if (root->lchild) {
+        if (root->lchild->lchild) root->lsi += root->lchild->lchild->lsi;
+        if (root->lchild->rchild) root->lsi += root->lchild->rchild->lsi;
+    }
+    if (root->rchild) {
+        if (root->rchild->lchild) root->lsi += root->rchild->lchild->lsi;
+        if (root->rchild->rchild) root->lsi += root->rchild->rchild->lsi;
+    }
+    return 0;    
 }
 
 int lsi(node_t *root) {
     if (!root) return 0;
+    f(root);
     int rst = 0;
     if (root->lchild) rst += root->lchild->lsi;
     if (root->rchild) rst += root->rchild->lsi;
